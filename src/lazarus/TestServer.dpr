a06238043aae7b172e11cc5282590e678afcf474
Turrican <djkike@djkike.es>
2018-04-18 14:28:26 +0200

    Added Lazarus demostration project

---
+++ src/lazarus/TestServer.dpr
@@ -0,0 +1,28 @@
+program TestServer;
+
+{$IFDEF FPC}
+  {$MODE Delphi}
+{$ENDIF}
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+{$IFnDEF FPC}
+  System.SysUtils,
+{$ELSE}
+  SysUtils,
+{$ENDIF}
+  TNsRestFramework.Application.Service;
+
+begin
+  try
+    TApplicationService.Init('6060');
+    Writeln('HTTP Server is listening at port 6060. Press a Key to stop service');
+    Readln;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
